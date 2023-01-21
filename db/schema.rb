e721115867c200dcd8b544ab6714bdf38ee0baa6
Cory Fonteyne <corymfonteyne@gmail.com>
2023-01-21 15:08:41 -0500

    completed rapid app dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_190224) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_022104) do
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer "grade"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
